83ce8e6989a98bfe939e5d6ec986b3f46443c3eb
francisco <franciscogaletti@yahoo.com>
2020-06-11 03:03:00 -0500

    Add - AdvertApi - Models

---
+++ AdvertApi/Controllers/AdvertController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AdvertApi.Models;
+using AdvertApi.Services;
+using Amazon.DynamoDBv2;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AdvertApi.Controllers
+{
+    [Route("Adverts/v1")]
+    [ApiController]
+    public class AdvertController : ControllerBase
+    {
+        //18
+        private readonly IAdvertStorageService _advertStorageService;
+
+        public AdvertController(IAdvertStorageService advertStorageService)
+        {
+            this._advertStorageService = advertStorageService;
+        }
+
+
+
+        [HttpPost]
+        [Route("Create")]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(200, Type=typeof(CreateAdvertResponse))]
+        public async Task<IActionResult> Create(AdvertModel model)
+        {
+            string recordId;
+            try
+            {
+                 recordId = await _advertStorageService.Add(model);
+            }
+            catch (KeyNotFoundException )
+            {
+
+                return new NotFoundResult();
+            }
+            catch (Exception exception) 
+            {
+                return StatusCode(500, exception.Message);
+            }
+
+            return StatusCode(201, new CreateAdvertResponse { Id = recordId });
+        }
+
+        // success upload S3
+        [HttpPut]
+        [Route("Confirm")]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(200)]
+        public async Task<IActionResult> Confirm(ConfirmAdvertModel model)
+        {
+            try
+            {
+                await _advertStorageService.Confirm(model);
+            }
+            catch (KeyNotFoundException)
+            {
+                return new NotFoundResult();
+            }
+            catch (Exception exception)
+            {
+                return StatusCode(500, exception.Message);
+            }
+
+            return new OkResult();
+        }
+    }
+}
---
+++ AdvertApi/Services/DynamoDBAdvertStorage.cs
@@ -0,0 +1,65 @@
+﻿using AdvertApi.Models;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DataModel;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdvertApi.Services
+{
+    //17
+    public class DynamoDBAdvertStorage : IAdvertStorageService
+    {
+        private readonly IMapper _mapper;
+        public DynamoDBAdvertStorage(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
+        public async Task<string> Add(AdvertModel model)
+        {
+            // optimistic just for the example
+            var dbMoled = _mapper.Map<AdvertDbModel>(model);
+            dbMoled.Id = new Guid().ToString();
+            dbMoled.CreationDateTime = DateTime.UtcNow;
+            dbMoled.Status = AdvertStatus.Pending;
+
+
+            using (var client = new  AmazonDynamoDBClient())
+            {
+                using (var context = new DynamoDBContext(client))
+                {
+                    await context.SaveAsync(dbMoled);
+                }
+            }
+
+            return dbMoled.Id;
+        }
+
+        // 18
+        public async Task Confirm(ConfirmAdvertModel model)
+        {
+            using (var client = new AmazonDynamoDBClient())
+            {
+                using (var context = new DynamoDBContext(client))
+                {
+                    var record = await context.LoadAsync<AdvertDbModel>(model.Id);
+                    if (record == null)
+                    {
+                        throw new KeyNotFoundException($"A record with ID= {model.Id } was not found.");
+                    }
+                    if (model.Status == AdvertStatus.Active)
+                    {
+                        record.Status = AdvertStatus.Active;
+                        await context.SaveAsync(record);
+                    }
+                    else
+                    {
+                        await context.DeleteAsync(record);
+                    }
+                }
+            }
+        }
+    }
+}
